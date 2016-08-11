58d0bdc0f11befa312a74a8881d5d25543cd593c
inhortte <inhortte@gmail.com>
2016-08-11 12:40:54 +0200

    Static index.html served by hapi

--- thurk/src/serv/dreadache.js
+++ thurk/src/serv/dreadache.js
@@ -1,13 +1,37 @@
 'use strict';
 
 import Hapi from 'hapi';
+import Inert from 'inert';
+import path from 'path';
 import { update } from './store';
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: path.join(__dirname, '../public')
+      }
+    }
+  }
+});
 server.connection({
-  port: 3049
+  port: process.env.DREADACHE_PORT || 3049
+});
+server.register(Inert, () => {});
+
+server.route({
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: '.',
+      redirectToSlash: true,
+      index: true
+    }
+  }
 });
 
+/*
 server.route({
   method: 'POST',
   path: '/revent',
@@ -16,6 +40,7 @@ server.route({
     reply('beat me senseless');
   }
 });
+*/
 
 server.start(err => {
   if(err) { throw err; }
